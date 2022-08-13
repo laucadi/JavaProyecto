4e04d24005c1ab54c0bbec80cfdb41c061d9fb86
Laura Caicedo <64761438+laucadi@users.noreply.github.com>
2022-08-12 22:55:17 -0500

    reto terminado

---
+++ Reto50G70/src/main/java/com/mycompany/reto50g70/controlador/ControladorMain.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.reto50g70.controlador;
+
+import com.mycompany.reto50g70.vista.frmPrincipal;
+
+
+
+
+public class ControladorMain {
+    public static void main(String[] args) {
+        new frmPrincipal().setVisible(true);
+    }
+    
+}
